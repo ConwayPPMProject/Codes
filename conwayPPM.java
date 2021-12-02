9034fe6934a16bf9187cb880474135f71de63e18
ConwayPPMProject <93350825+ConwayPPMProject@users.noreply.github.com>
2021-12-02 14:05:47 -0500

    Pow 2 checker implemented
    the code will now return based on how many cycles you want it to do. It will tell you the output of the machine, the step of the cycle it exited on, what cycle it is on, if the number is equivalent to a power of 2, and what that power of 2 is.

--- conwayPPM.java
+++ conwayPPM.java
@@ -7,74 +7,77 @@ public class Main
        
         //machine loop
         System.out.println("start");
-           while (initial != 15)
+           while (count < 100)
             {
                 if(((initial * 17) / 91) % 1 == 0)
                 {
                     initial = (initial * 17) / 91;
-                    System.out.println("1");
+                    System.out.println("Step: 1");
                 } else if(((initial * 78) / 85) % 1 == 0)
                 {
                     initial = (initial * 78) / 85;
-                    System.out.println("2");
+                    System.out.println("Step: 2");
                 } else if(((initial * 19) / 51) % 1 == 0)
                 {
                     initial = (initial * 19) / 51;
-                    System.out.println("3");
+                    System.out.println("Step: 3");
                 } else if(((initial * 23) / 38) % 1 == 0)
                 {
                     initial = (initial * 23) / 38;
-                    System.out.println("4");
+                    System.out.println("Step: 4");
                 } else if(((initial * 29) / 33) % 1 == 0)
                 {
                     initial = (initial * 29) / 33;
-                    System.out.println("5");
+                    System.out.println("Step: 5");
                 } else if(((initial * 77) / 29) % 1 == 0)
                 {
                     initial = (initial * 77) / 29;
-                    System.out.println("6");
+                    System.out.println("Step: 6");
                 } else if(((initial * 95) / 23) % 1 == 0)
                 {
                     initial = (initial * 95) / 23;
-                    System.out.println("7");
+                    System.out.println("Step: 7");
                 } else if(((initial * 77) / 19) % 1 ==0)
                 {
                     initial = (initial * 77) / 19;
-                    System.out.println("8");
+                    System.out.println("Step: 8");
                 } else if(((initial * 1) / 17) % 1 == 0)
                 {
                     initial = initial / 17;
-                    System.out.println("9");
+                    System.out.println("Step: 9");
                 } else if(((initial * 11) / 13) % 1 == 0)
                 {
                     initial = (initial * 11) / 13;
-                    System.out.println("10");
+                    System.out.println("Step: 10");
                 } else if(((initial * 13) / 11) % 1 == 0)
                 {
                     initial = (initial * 13) / 11;
-                    System.out.println("11");
+                    System.out.println("Step: 11");
                 } else if(((initial * 15) / 14) % 1 == 0)
                 {
                     initial = (initial * 15) / 14;
-                    System.out.println("12");
+                    System.out.println("Step: 12");
                 } else if(((initial * 15) / 2) % 1 ==0)
                 {
                     initial = (initial * 15) / 2;
-                    System.out.println("13");
+                    System.out.println("Step: 13");
                 } else if(((initial * 55) / 1) % 1 ==0)
                 {
                     initial = (initial * 55);
-                    System.out.println("14");
+                    System.out.println("Step: 14");
                 }
-                System.out.println(initial);
                 count++;
+                //print statements
+                int output = (int)initial;
+                System.out.println("--------------------\nOutput: " + output + "\nRevolution: " + count);
+                //pow2 checker
+                for (int o = 0; o<output; o++)
+                    {
+                        if(Math.pow(2.0, o) == output)
+                    {
+                        System.out.println("power of 2: " + o);
+                    }
+                }
             }
-        //print statements
-        int output = (int)initial;
-        System.out.println("--------------------\nOutput: " + output + "\nRevolution: " + count);
-        
-        //pow2 checker
     }
-
     }
-
